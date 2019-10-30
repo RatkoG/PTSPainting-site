5d36e731be3aa676a3574bbc35dfc021e379f3f3
ratko <ratko.gjurichanin@gmail.com>
2019-10-30 11:06:34 -0600

    minor changes stable code

---
+++ dist/app.js
@@ -0,0 +1,9 @@
+document.querySelector('.btn--green').addEventListener('click', function () {
+  console.log('Working');
+});
+
+const img = document
+  .querySelector('.gallery__item--1')
+  .addEventListener('click', function () {
+    console.log('image!');
+  });
