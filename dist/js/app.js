2ffa514dfaa9e20ea4cb5d787e768b50805ad60e
ratko <ratko.gjurichanin@gmail.com>
2019-11-07 17:35:01 -0600

    So far so good
    Checkbox hack is not working properly

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,10 +1,12 @@
 const modal = document.querySelector('#modalForm');
 const modalBtn = document.querySelector('#modalBtn');
 const closeBtn = document.querySelector('.closeBtn');
+// const hamburger = document.querySelector('.hamburger');
 
 modalBtn.addEventListener('click', openModal);
 closeBtn.addEventListener('click', closeModal);
 window.addEventListener('click', clickOutside);
+hamburger.addEventListener('click', openMenu)
 
 function openModal() {
   modal.style.display = 'block';
@@ -19,6 +21,11 @@ function clickOutside(e) {
     modal.style.display = 'none';
   }
 }
+
+// function openMenu() {
+//   // navLinks.classList.toggle('open');
+//   hamburger.classList.toggle('is-active');
+// }
 // document.querySelector('.btn--green').addEventListener('click', function () {
 //   console.log('Working');
 // });
